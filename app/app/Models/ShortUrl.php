62d05d6ca6c3f92a4b5f21526faf14b13d6cf609
mortyje <mortyje@inbox.ru>
2026-07-05 11:19:41 +0300

    feat: add model relationships

--- app/app/Models/ShortUrl.php
+++ app/app/Models/ShortUrl.php
@@ -2,10 +2,30 @@
 
 namespace App\Models;
 
-use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class ShortUrl extends Model
 {
-    use HasFactory;
+    protected $fillable = [
+        'user_id',
+        'original_url',
+        'short_code',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function clicks(): HasMany
+    {
+        return $this->hasMany(Click::class);
+    }
+
+    public function getClicksCountAttribute(): int
+    {
+        return $this->clicks()->count();
+    }
 }
